c513e85f45c659deb3106db331174838e4f5ca88
tkt181.lee <tkt181.lee@gmail.com>
2026-08-05 16:51:31 +0800

    Fix layout error and update website URL in admin about page

--- admin/about.php
+++ admin/about.php
@@ -4,10 +4,15 @@
 require __DIR__ . '/header.php';
 //xoops_cp_header();
 
+
 $adminObject = \Xmf\Module\Admin::getInstance();
 
 $adminObject->displayNavigation(basename(__FILE__));
 $adminObject::setPaypal('xoopsfoundation@gmail.com');
+
+// 修正 Xmf Admin displayAbout 排版錯置（#about label 浮動問題）
+echo '<style>#about label, #about text { float: none; display: inline-block; width: auto; text-align: left; padding-right: 0; }</style>';
+
 $adminObject->displayAbout(false);
 
 require __DIR__ . '/footer.php';
